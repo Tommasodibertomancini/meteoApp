f040e9d0af1b645bf1c6dfdd3210453528442c1f
Tommasodibertomancini <tommydbm@gmail.com>
2025-01-17 16:53:30 +0100

    wehaterdefault

--- src/components/WheaterDefault.jsx
+++ src/components/WheaterDefault.jsx
@@ -5,38 +5,66 @@ function WheaterDefault(props) {
   return (
     <Container fluid>
       <Row>
-        <Col xs={12}>
-          {!props.searched ? (
-            <>
+        {!props.searched ? (
+          <>
+            <Col xs={12} lg={6} className='mb-3'>
               <WheaterHome
                 searched={'Rome'}
                 nation={'IT'}
                 handleId={props.handleId}
+                isFullPage={false}
               />
+            </Col>
+            <Col xs={12} lg={6} className='mb-3'>
               <WheaterHome
                 searched={'London'}
                 nation={'GB'}
                 handleId={props.handleId}
+                isFullPage={false}
               />
+            </Col>
+            <Col xs={12} lg={6} className='mb-3'>
               <WheaterHome
                 searched={'Los Angeles'}
                 nation={'US'}
                 handleId={props.handleId}
+                isFullPage={false}
               />
+            </Col>
+            <Col xs={12} lg={6} className='mb-3'>
               <WheaterHome
                 searched={'Madrid'}
                 nation={'ES'}
                 handleId={props.handleId}
+                isFullPage={false}
               />
-            </>
-          ) : (
-            <WheaterHome
-              searched={props.searched}
-              nation={props.nation}
-              handleId={props.handleId}
-            />
-          )}
-        </Col>
+            </Col>
+            <Col xs={12} lg={6} className='mb-3'>
+              <WheaterHome
+                searched={'Sydney'}
+                nation={'AU'}
+                handleId={props.handleId}
+                isFullPage={false}
+              />
+            </Col>
+            <Col xs={12} lg={6} className='mb-3'>
+              <WheaterHome
+                searched={'Tokyo'}
+                nation={'JP'}
+                handleId={props.handleId}
+                isFullPage={false}
+              />
+            </Col>
+            
+          </>
+        ) : (
+          <WheaterHome
+            searched={props.searched}
+            nation={props.nation}
+            handleId={props.handleId}
+            isFullPage={true}
+          />
+        )}
       </Row>
     </Container>
   );
